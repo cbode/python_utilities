f54c6de45b2a43663fff879ac35324de299b9921
Collin Bode <collin@berkeley.edu>
2016-08-02 10:25:51 -0700

    refactored to functions
    changed code form inline to functions that can be called separately.

--- utilities_time.py
+++ utilities_time.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
 ################################################################################
-# name: utilities_time.py
+# name: utilities.py
 # author: collin bode, email: collin@berkeley.edu
 # date: 2016-05-09
-#  added to github
-# purpose: Functions for the manipulation of timeseries data
+#
+# purpose: Functions for the manipulation of timeseries data and files.
 ################################################################################
 
 import os
 import datetime as dt
 import pandas as pd
-from utilities_file import get_filepath
 
+# Get absolute path to this file
+def get_filepath():
+    path = os.path.dirname(__file__)
+    path = path+os.sep
+    return path
+    
+# Determine if text is a timestamp without crashing
 def is_datetime(dtstring):
     if dtstring.startswith('"') and dtstring.endswith('"'):
         dtstring = dtstring[1:-1]
@@ -24,8 +30,8 @@ def is_datetime(dtstring):
     except:
         return False
 
-def get_header_row_length(path,delim,verbose):
-    # Determine how many rows of header
+# Determine how many rows in the header
+def get_header_row_length(fpath,delim,verbose):
     fin = open(fpath,'r')
     i = 0
     for row in fin:
@@ -42,30 +48,38 @@ def get_header_row_length(path,delim,verbose):
     fin.close()
     return i
 
-def get_duplicate_timestamp_count(path,delim):
-    header_length = get_header_row_length(path,delim,False)
-    df = pd.read_csv(fpath,sep=delim,header=None,prefix='c',skiprows=header_length,parse_dates=[0])
+# Opens timeseries file and creates Pandas DataFrame
+def create_timeseries_dataframe(fpath,delim,date_as_index):
+    header_length = get_header_row_length(fpath,delim,False)
+    if(date_as_index == True):
+        df = pd.read_csv(fpath,sep=delim,header=None,prefix='c',skiprows=header_length,parse_dates=0, date_parser=pd.datetools.datetime,index_col=0)        
+    else:
+        df = pd.read_csv(fpath,sep=delim,header=None,prefix='c',skiprows=header_length,parse_dates=0, date_parser=pd.datetools.datetime,dtype={0:dt.datetime},low_memory=False)
+    return df
+
+def get_duplicate_timestamp_count(df):
     df['dup'] = df.duplicated(subset='c0')
     return df['dup'].sum()    
 
-def return_duplicate_timestamps(path,delim,both):
+def return_duplicate_timestamps(df,both,allfields):
     # both = True.  Return both the original and the duplicate row.
     # both = False means you just return the second or more rows, not the original.
-    header_length = get_header_row_length(path,delim,False)
-    df = pd.read_csv(fpath,sep=delim,header=None,prefix='c',skiprows=header_length,parse_dates=[0])
-    if(both == True):
-        #dupsplus = pd.merge(dups,df,how='left',left_on='c0',right_on='c0')
+    # allfields = True means every field will be compared to determine duplicate.  This will leave duplicate timestamps if the fields are different.
+    # allfields = False means only the timestamp will be considered.
+    if(both == True and allfields == True):
         df['dup'] = df.duplicated(subset='c0',keep=False)
+    elif(both == True and allfields == False):
+        df['dup'] = df.duplicated(keep=False)
+    elif(both == False and allfields == True):
+        df['dup'] = df.duplicated(subset='c0')    
     else:
-        df['dup'] = df.duplicated(subset='c0')
+        df['dup'] = df.duplicated()
     dups = df[df['dup'] == True]
     return dups
     
-def remove_duplicate_timestamps(path,delim,allfields):
+def remove_duplicate_timestamps(df,allfields):
     # allfields = False means only the timestamp will be considered when removing duplicates.
     # allfields = True means every field will be compared to determine duplicate.  This will leave duplicate timestamps if the fields are different.
-    header_length = get_header_row_length(path,delim,False)
-    df = pd.read_csv(fpath,sep=delim,header=None,prefix='c',skiprows=header_length,parse_dates=[0])
     if(allfields == False):
         df['dup'] = df.duplicated(subset='c0')
     else:
@@ -73,7 +87,16 @@ def remove_duplicate_timestamps(path,delim,allfields):
     dfclean = df.drop(df[df.dup == True].index)
     return dfclean
 
-
+def check_timeseries_gaps(df,interval):
+    # interval should be in minutes, e.g. '10Min'
+    # note: requires no duplicate timestamps
+    dfclean = remove_duplicate_timestamps(df,True)
+    dfclean.set_index('c0',inplace=True)    
+    mindate = dfclean.index.min()        
+    maxdate = dfclean.index.max()
+    ti = pd.date_range(start=mindate,end=maxdate,freq=interval)
+    
+    
 ################################    
 # Main runs test cases
 
@@ -82,7 +105,9 @@ path = get_filepath()
 datfile = 'dup_test.dat'
 fpath = path+datfile
 delim = ','
+df = create_timeseries_dataframe(fpath,delim,False)
 
+'''
 # Find out how many rows there are in the header
 header_length = get_header_row_length(fpath,delim,True)
 print('Header is ',header_length,' rows long')
@@ -92,14 +117,18 @@ number_dups = get_duplicate_timestamp_count(fpath,delim)
 print('Number of duplicate timestamps: ',number_dups)
 
 # List all duplicate timestamps 
-dups = return_duplicate_timestamps(fpath,delim,True)
+dups = return_duplicate_timestamps(fpath,delim,True,True)
 print('All timestamps that are duplicate.  Will return rows with different other fields, if timestamp is the same.')
 print(dups)
 
 # List all perfectly duplicated rows 
-dups = return_duplicate_timestamps(fpath,delim,False)
+dups = return_duplicate_timestamps(fpath,delim,False,True)
 print('Rows that are duplicate in all fields.')
 print(dups)
 
 # Delete Duplicates from dataset 
 dfclean = remove_duplicate_timestamps(path,delim,False)
+print(dfclean)
+'''
+
+# Check Timeseries for gaps
